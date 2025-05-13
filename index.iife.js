5bee88e89d5eb6ef1c4c7f29d0d96ba6f4151f06
ahroncich7 <alehroncich@gmail.com>
2025-05-13 15:19:45 -0300

    v0.0.5

--- index.iife.js
+++ index.iife.js
@@ -1,39 +1,23 @@
-(function(){ try {var elementStyle = document.createElement('style'); elementStyle.appendChild(document.createTextNode("")); document.head.appendChild(elementStyle);} catch(e) {console.error('vite-plugin-css-injected-by-js', e);} })();(function(){"use strict";const E=async(n,a,o)=>(await(await fetch("https://cichautomation.duckdns.org/webhook/9a97cd47-cd26-47ae-91f8-b92d0a62ee5a",{method:"POST",body:JSON.stringify({text:n,sender:a,id:o,timestamp:new Date})})).json()).content,M=async n=>(await(await fetch(`https://cichautomation.duckdns.org/webhook/9a97cd47-cd26-47ae-91f8-b92d0a62ee5a?id=${n}`)).json()).content,u=(n,a)=>`
-                    <div class="flex w-full mt-2 space-x-3 max-w-xs justify-start flex-col">
-						<div>
-							<div class="bg-zinc-200 text-gray-500 p-3 rounded-r-lg rounded-bl-lg">
-								<p class="text-sm">${n}</p>
-							</div>
-							<span data-timestamp='${a}' class="messageTimeStamp text-xs text-gray-500 leading-none">2 min ago</span>
-						</div>
-				    </div>`,p=(n,a,o)=>`<div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-					<div>
-						<div class="bg-${o}-200 text-gray-500 p-3 rounded-l-lg rounded-br-lg">
-							<p class="text-sm">${n}</p>
-						</div>
-						<span data-timestamp='${a}' class="messageTimeStamp text-xs text-gray-500 leading-none">2 min ago</span>
-					</div>
-				</div>`;(async()=>{const n=document.getElementById("AlcLabsScript").getAttribute("data-theme")||"sky",a=document.getElementById("AlcLabsScript").getAttribute("data-apikey"),o=document.createElement("div");document.body.appendChild(o);const c=o.attachShadow({mode:"open"});let l=c;const g="fixed top-0 pt-20 w-full px-4 z-50 transition-all duration-300 flex justify-center",C="fixed bottom-4 right-4 z-50 transition-all duration-300",h=document.createElement("style"),T="https://cdn.jsdelivr.net/gh/ahroncich7/augmentumResources@v0.0.4/styles.css";h.textContent=await fetch(T).then(t=>t.text());const i=document.createElement("div");i.className=g;let r=!0;const B=(t,s)=>`
-     <div class="flex flex-col flex-grow w-full max-w-xl shadow-xl rounded-3xl overflow-hidden">
-       <div class="w-full bg-${s}-300/60 backdrop-blur-sm z-10 flex shadow-lg">
-         <button id="close-widget" class="text-zinc-600 hover:text-black text-sm ms-auto me-5 py-1 my-1">
-           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-x"><path d="M6 6l12 12M6 18L18 6"/></svg>
-         </button>
-       </div>
-       <div class="bg-zinc-50/80 backdrop-blur-sm p-2">
-         <div id="chat-container" class="flex flex-col flex-grow h-0 p-4 overflow-auto min-h-40 z-0">
-           ${t.map(e=>e.sender==="user"?p(e.text,e.timestamp,s):u(e.text,e.timestamp)).join("")}
+(function(){ try {var elementStyle = document.createElement('style'); elementStyle.appendChild(document.createTextNode("")); document.head.appendChild(elementStyle);} catch(e) {console.error('vite-plugin-css-injected-by-js', e);} })();(function(){"use strict";const M=async(a,s,d,c)=>(await(await fetch("https://cichautomation.duckdns.org/webhook/9a97cd47-cd26-47ae-91f8-b92d0a62ee5a",{method:"POST",body:JSON.stringify({id:c,newMessage:{text:a,sender:s,timestamp:new Date},history:d})})).json()).content,f=(a,s)=>`<div class="flex w-full mt-2 mb-4 text-white px-5">
+				<p class="text-sm">${a}</p>		
+			</div>`,I=()=>`<div id="skeleton-message" class="w-4/5 max-w-sm p-2 flex flex-col gap-2">	
+				<div class="animate-pulse h-3 bg-gray-300 rounded w-1/3"></div>
+				<div class="animate-pulse h-3 bg-gray-300 rounded w-full"></div>
+				<div class="animate-pulse h-3 bg-gray-300 rounded w-3/4"></div>	
+  			</div>`,w=(a,s)=>`<div class="flex mt-2 mb-4 w-full text-white justify-center">
+				<p class="text-lg">${a}</p>		
+			</div>`,T=a=>`<div id="widget-container" class="bg-transparent relative rounded-3xl flex flex-col justify-center align-middle w-full max-w-screen-md transition-all duration-200">
+			<button id="minimize-button" class="absolute z-50 right-5 top-2 ml-auto text-white">
+				<svg xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-minus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l14 0" /></svg>
+			</button>
+		 <div id="chat-container" class="flex flex-col flex-grow overflow-auto max-h-0 z-0 transition-all duration-200">
+           ${a.map(s=>s.sender==="user"?w(s.text,s.timestamp):f(s.text,s.timestamp)).join("")}
          </div>
-       </div>
-       <div class="bg-zinc-100/80 backdrop-blur-sm p-4 z-30 shadow-lg-top flex flex-row">
-         <textarea id="chat-input" class="flex bg-transparent items-center h-10 w-full rounded px-3 text-sm focus-visible:outline-none text-gray-500" placeholder="Type your message…"></textarea>
-         <button id="sendButton" class="text-${s}-500 flex flex-col justify-center">
-           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-send"><path d="M10 14l11-11M21 3l-6.5 18a.55.55 0 01-1 0l-3.5-7-7-3.5a.55.55 0 010-1l18-6.5"/></svg>
-         </button>
-       </div>
-     </div>
-   `,j=`
-  <div id="chat-bubble" class="bg-${n}-500 text-white rounded-full w-16 h-16 flex items-center justify-center shadow-lg cursor-pointer hover:bg-${n}-600 transition-colors">
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icon-tabler-chart-bubble"><path d="M6 16m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"/><path d="M16 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"/><path d="M14.5 7.5m-4.5 0a4.5 4.5 0 1 0 9 0a4.5 4.5 0 1 0 -9 0"/></svg>
-  </div>
-`,x=()=>{i.className=g,i.innerHTML=B(b,n),requestAnimationFrame(()=>{$(),f(),k()})},S=()=>{i.className=C,i.innerHTML=j;const t=i.querySelector("#chat-bubble");t==null||t.addEventListener("click",w)},w=()=>{r?S():x(),r=!r},$=()=>{var s,e;const t=c;(s=t.getElementById("sendButton"))==null||s.addEventListener("click",v),(e=t.getElementById("close-widget"))==null||e.addEventListener("click",w)},v=async()=>{const t=document.getElementById("chat-input"),s=t.value;if(!s)return;y({text:s,sender:"user",timestamp:new Date().toString()}),t.value="";const e=await E(s,"user",a);y(e)},f=()=>{const t=new Date;l.querySelectorAll(".messageTimeStamp").forEach(e=>{const d=new Date(e.dataset.timestamp);if(!d)return;const m=Math.ceil((t-d)/(1e3*60));e.innerHTML=`Hace ${m} min.`})},b=await M(a);c.appendChild(h),c.appendChild(i),x(),l.addEventListener("keydown",t=>{r&&t.key==="Enter"&&!t.shiftKey&&(t.preventDefault(),v())});const y=t=>{const s=l.getElementById("chat-container"),e=t.sender=="user"?p(t.text,t.timestamp,n):u(t.text,t.timestamp),d=l.createElement("div");d.innerHTML=e;const m=d.firstElementChild;s.appendChild(m),b.push(t),requestAnimationFrame(()=>{k(),f()})},k=()=>{const t=l.getElementById("chat-container");t.scrollTo({top:t.scrollHeight,behavior:"smooth"})}})()})();
+		<div id="input-container" class="${q.join(" ")} p-6 rounded-3xl text-white flex justify-between align-middle mx-auto w-full transition-all duration-200">
+			<input id="chat-input" class="w-full placeholder:text-gray-100 bg-transparent ${H.join(" ")} focus-visible:outline-none max-w-screen-sm" placeholder="Preguntame lo que quieras"></input>
+			<button id="send-button" class="my-auto invisible hover:text-slate-300">
+				<svg xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-send-2"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4.698 4.034l16.302 7.966l-16.302 7.966a.503 .503 0 0 1 -.546 -.124a.555 .555 0 0 1 -.12 -.568l2.468 -7.274l-2.468 -7.274a.555 .555 0 0 1 .12 -.568a.503 .503 0 0 1 .546 -.124z" /><path d="M6.5 12h14.5" /></svg>
+			</button>
+		</div>
+		<a id="alchimiaLabs" href="https://localhost" target="_blank" class="ms-auto hidden"><p class="text-slate-200 hover:text-slate-50 italic text-xs cursor-pointer">powered by <span class="not-italic text-sm">AlchimiaLabs</span></p></a>
+	  </div>`,q=["bg-gray-700/80"],H=["text-lg","md:text-2xl"],j=`fixed ${["top-1/2","md-top-1/5","-translate-y-1/2","md:translate-y-0"].join(" ")} flex justify-center w-screen z-50 transition-all duration-300 px-5`;(async()=>{const a=[{text:"Hola. ¿Cómo podemos ayudarte?",sender:"system",timestamp:new Date}];document.getElementById("AlcLabsScript").getAttribute("data-theme");const s=document.getElementById("AlcLabsScript").getAttribute("data-apikey"),d=document.createElement("div");document.body.appendChild(d);const c=d.attachShadow({mode:"open"});let t=c;const h=document.createElement("style"),B="styles.css";h.textContent=await fetch(B).then(e=>e.text());const o=document.createElement("div");let v,x,y,b,u,l;const D=()=>{o.className=j,o.innerHTML=T(m),v=t.querySelector("#chat-container"),t.querySelector("#widget-container"),b=t.querySelector("#input-container"),u=t.querySelector("#chat-input"),x=t.querySelector("#send-button"),y=t.querySelector("#minimize-button"),t.querySelector("#alchimiaLabs"),z(),l=1},z=()=>{x.addEventListener("click",k),y.addEventListener("click",P),t.addEventListener("scroll",O),b.addEventListener("click",J)},k=async()=>{const e=u.value;if(r(2),!e)return;const n=[...m];S({text:e,sender:"user",timestamp:new Date().toString()}),L(!0),u.value="";const i=await M(e,"user",n,s);L(!1),S(i)},S=e=>{t.getElementById("chat-container");const n=e.sender=="user"?w(e.text,e.timestamp):f(e.text,e.timestamp);C(n),m.push(e),$("messages",m,36e5),requestAnimationFrame(()=>{E(),A()})},A=()=>{const e=new Date;t.querySelectorAll(".messageTimeStamp").forEach(i=>{const p=new Date(i.dataset.timestamp);if(!p)return;const g=Math.ceil((e-p)/(1e3*60));i.innerHTML=`Hace ${g} min.`})},m=N("messages")||a;c.appendChild(h),c.appendChild(o),D(),t.addEventListener("keydown",e=>{t.activeElement==u&&(l!=2&&r(2),e.key==="Enter"&&!e.shiftKey&&(e.preventDefault(),k()))});const C=e=>{const n=t.createElement("div");n.innerHTML=e;const i=n.firstElementChild;v.appendChild(i)},E=()=>{const e=t.getElementById("chat-container");e.scrollTo({top:e.scrollHeight,behavior:"smooth"})};function $(e,n,i){const g={value:n,expiry:new Date().getTime()+i};sessionStorage.setItem(e,JSON.stringify(g))}function N(e){const n=sessionStorage.getItem(e);if(!n)return null;const i=JSON.parse(n);return new Date().getTime()>i.expiry?(sessionStorage.removeItem(e),null):i.value}function O(){window.scrollY>window.visualViewport.height*.2&&r(3),window.scrollY==0&&r(1)}const L=e=>{e?C(I()):t.querySelector("#skeleton-message").remove()};function J(e){e.preventDefault(),l!=2&&r(2)}function P(e){e.preventDefault(),l==2?r(1):l==1&&r(3)}function r(e){e==1?(o.classList.remove("expanded"),o.classList.remove("minified"),l=1):e==2?(o.classList.remove("minified"),o.classList.add("expanded"),E(),l=2):e==3&&(o.classList.remove("expanded"),o.classList.add("minified"),l=3)}})()})();
